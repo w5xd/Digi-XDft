e077851d7ea1812e3c66aa10d384bcb60bf48c4f
Wayne Wright <w5xd@alum.mit.edu>
2020-02-11 06:43:25 -0800

    Beta (#1)
    * update submodule
    
    * update submodule

--- XDft/XDftVersion.cs
+++ XDft/XDftVersion.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("XDft8")]
-[assembly: AssemblyCopyright("Copyright © 2019 Wayne E. Wright, W5XD 2019")]
+[assembly: AssemblyCopyright("Copyright © 2020 Wayne E. Wright, W5XD 2019")]
 
 
-[assembly: AssemblyVersion("1.0.0.10")]
-[assembly: AssemblyFileVersion("1.0.0.10")]
+[assembly: AssemblyVersion("1.0.0.11")]
+[assembly: AssemblyFileVersion("1.0.0.11")]
